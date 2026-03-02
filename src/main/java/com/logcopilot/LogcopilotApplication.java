17c09506b0055f487ea89efb784f18365f3fb037
hyoseok <alexization@gmail.com>
2026-03-03 01:19:51 +0900

    init: initial project

---
+++ src/main/java/com/logcopilot/LogcopilotApplication.java
@@ -0,0 +1,13 @@
+package com.logcopilot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogcopilotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogcopilotApplication.class, args);
+	}
+
+}
